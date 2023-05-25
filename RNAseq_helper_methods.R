92e5744beffac927a1601110f09df249666ca9cb
nehmea <79180098+nehmea@users.noreply.github.com>
2023-05-25 15:33:28 -0400

    Update RNAseq_helper_methods.R
    commented calculate_ratio() function

--- RNAseq_helper_methods.R
+++ RNAseq_helper_methods.R
@@ -65,31 +65,65 @@ mergeCounts = function(files, header=T, geneCol, countsCol, sampleNames=NULL){
 
 
 
-#calculates ratios between paired samples in an DESeq2ExpressionSet
+# Calculates ratios between paired samples in a DESeq2ExpressionSet
+# This function calculates the ratios between paired samples in a DESeq2ExpressionSet.
+# 
+# Arguments:
+#   
+    # eset: DESeq2ExpressionSet object containing the gene expression data.
+    # is_log2: Logical value indicating whether the data is already in log2 scale.
+    # group_names: Character vector specifying the group names.
+    # groups_col: Column name in the eset object containing the group information.
+    # conditions_col: Column name in the eset object containing the condition information.
+    # primary_condition: Name of the primary condition for ratio calculation.
+    # ref_condition: Name of the reference condition for ratio calculation.
+# Returns:
+    # data frame with gene names as row names and ratios between paired samples as columns.
+
+
 calculate_ratios = function(eset, is_log2, group_names, groups_col, conditions_col, primary_condition, ref_condition) {
   require(progress)
+  
+  # Create a progress bar
   pb = progress_bar$new(format = "(:spin) [:bar] :percent [Elapsed time: :elapsedfull || Estimated time remaining: :eta]",
-                        total = ncol(dds),
+                        total = ncol(eset),
                         complete = "=",   # Completion bar character
                         incomplete = "-", # Incomplete bar character
                         current = ">",    # Current bar character
                         clear = FALSE,    # If TRUE, clears the bar when finish
                         width = 100)      # Width of the progress bar
   
+  # Create a data frame to store the differences
   difference = data.frame(genes = rownames(eset))
-  if(!is_log2) {assay(eset) = log2(assay(eset) + 1)}
-  for(group in group_names){
-    sub_eset = eset[,eset[[groups_col]] == group]
+  
+  # Check if the data needs to be transformed to log2 scale
+  if(!is_log2) {
+    assay(eset) = log2(assay(eset) + 1)
+  }
+  
+  # Iterate over each group
+  for(group in group_names) {
+    sub_eset = eset[, eset[[groups_col]] == group]
     
+    # Check if the subset has two columns and contains the primary and reference conditions
     if(dim(sub_eset)[2] == 2 & all(sub_eset[[conditions_col]] %in% c(primary_condition, ref_condition))) {
+      # Calculate the difference between primary and reference conditions
       difference[group] = assay(sub_eset[, sub_eset[[conditions_col]] == primary_condition]) - assay(sub_eset[, sub_eset[[conditions_col]] == ref_condition])
     }
-    pb$tick()
+    
+    pb$tick()  # Update the progress bar
   }
+  
   row.names(difference) = difference$genes
   difference = difference[, -1]
-  if(!is_log2) {difference = 2^difference}
+  
+  # Transform the differences back to original scale if needed
+  if(!is_log2) {
+    difference = 2^difference
+  }
+  
   return(difference)
 }
+
 
 
