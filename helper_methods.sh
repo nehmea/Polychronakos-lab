ee0be0b458eaa4984d9c9ef23cc62496f2c5a523
nehmea <79180098+nehmea@users.noreply.github.com>
2023-08-08 11:07:57 -0400

    Update helper_methods.sh
    added plink scoring command
    added command to extract specific snp Ids from vcf file

--- helper_methods.sh
+++ helper_methods.sh
@@ -1,4 +1,4 @@
-#extract columns and rows from vcf file
+#extract columns and rows from vcf file for specific rsIds
 bcftools view --include ID==@grs2_alleles_rsIds.list ../dbsnp-All-hg38.vcf.gz --threads 6 | bcftools query -f '%CHROM\t%POS\t%ID\t%REF\t%ALT\n' > grs2-alleles-info.txt
 
 #recode plink bed to vcf
@@ -11,9 +11,16 @@ tabix -p vcf [rsId_pos_file]".gz"
 bcftools annotate [vcf_file] --annotations [rsId_pos_file]".gz" -c CHROM,POS,ID -Oz -o [output_file] --threads 6
 
 #extract rows with specific IDs from vcf file
-bcftools view -i 'ID=@id_file.txt' [input_file] -Oz -o [output_file]
+bcftools view -i 'ID=@id_file.txt' [input_file] -Oz -o [output_file] --threads 6
+
+#extract rows with specific positions from vcf file
+#snp_pos_file must include at least CHROM and POS columns
+bcftools view [input_file] -R [snp_pos_file] -Oz -o [output_file] --threads 6
 
 #plink score calculation
-plink2 --vcf t1dgc_grs2-snps_3971_rsIds.vcf.gz --score grs2_snp_betas_all_for_plink.txt 1 2 3 header list-variants cols=fid,pheno1,nallele,denom,dosagesum,scoreavgs,scoresums no-mean-imputation --out GRS2_3971_plink-score_no-imputation_use-alt_all-snps
+plink2 --vcf grs2_alleles_rsIds.vcf.gz --score grs2_snp_betas_all_for_plink.txt 1 2 3 header list-variants cols=fid,pheno1,nallele,denom,dosagesum,scoreavgs,scoresums no-mean-imputation ignore-dup-ids --out grs2_snp_score_plink_no-imputation_all-snps
+
+# Concatenate VCF files
+bcftools concat [input_files_list] -Oz -o [output_file]
 
 
